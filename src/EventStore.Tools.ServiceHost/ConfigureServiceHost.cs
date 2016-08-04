6061014886788036e00c813f6c1c783386054176
riccardo.dinuzzo@calastone.com <riccardo@dinuzzo.it>
2016-08-04 10:27:33 +0100

    Started the Servicestartup logic on a separate thread in order to keep it up and running when ServiceHost is installed as a service

--- src/EventStore.Tools.ServiceHost/ConfigureServiceHost.cs
+++ src/EventStore.Tools.ServiceHost/ConfigureServiceHost.cs
@@ -10,51 +10,28 @@ namespace EventStore.Tools.ServiceHost
 {
     public static class ConfigureServiceHost
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof(ConfigureServiceHost));
         public static void Configure()
         {
             if (ConfigurationManager.GetSection("log4net") != null)
                 XmlConfigurator.Configure();
             else
                 Logger.Setup();
-            var factories = GetStrategyFactories();
             HostFactory.Run(x =>
             {
                 x.UseLog4Net();
-                x.Service<ServiceStrategy>(s =>
+                x.Service<ServiceStartup>(s =>
                 {
-                    s.ConstructUsing(name => new ServiceStrategy(factories));
+                    s.ConstructUsing(name => new ServiceStartup());
                     s.WhenStarted(
                         (tc, hostControl) =>
-                            tc.Start());
+                            tc.Start(hostControl));
                     s.WhenStopped(tc => tc.Stop());
                 });
-                x.RunAsLocalSystem();
                 x.StartAutomatically();
                 x.SetDescription("This process load and run Application Service modules from the plugins directory");
                 x.SetDisplayName("EventStore Tools ServiceHost");
                 x.SetServiceName("EventStore.Tools.ServiceHost");
             });
-        }
-
-        private static IServiceStrategyFactory[] GetStrategyFactories()
-        {
-            var pluginsFolder = GetPluginsFolder();
-            PluginLoader.LoadPlugins(pluginsFolder, Log);
-            return (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
-                          from assemblyType in domainAssembly.GetTypes()
-                          where typeof(IServiceStrategyFactory).IsAssignableFrom(assemblyType) && assemblyType.IsClass
-                          select assemblyType).ToArray().Select(Activator.CreateInstance)
-                .Select(instance => instance)
-                .Cast<IServiceStrategyFactory>()
-                .ToArray();
-        }
-
-        private static string GetPluginsFolder()
-        {
-            var setting = ConfigurationManager.AppSettings["PluginsFolder"] ?? "plugins";
-            var uri = new Uri(setting, UriKind.RelativeOrAbsolute);
-            return uri.IsAbsoluteUri ? uri.AbsolutePath : setting;
         }
     }
 }
---
+++ src/EventStore.Tools.ServiceHost/ServiceStartup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using EventStore.Tools.PluginModel;
+using log4net;
+using Topshelf;
+
+namespace EventStore.Tools.ServiceHost
+{
+    public class ServiceStartup 
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(ServiceStartup));
+        private HostControl _hostControl;
+
+        public ServiceStartup() { }
+
+        public void Stop()
+        {
+            _hostControl.Stop();
+            Log.Info("ServiceHost stopped");
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            _hostControl = hostControl;
+            var myThread = new Thread(Init) {IsBackground = true};
+            myThread.Start();
+            return true;
+        }
+
+        private static void Init()
+        {
+            var factories = GetStrategyFactories();
+            foreach (var strategy in factories.Select(factory => factory.Create()))
+            {
+                strategy.Start();
+                Log.Info($"{strategy.GetType().Name} started");
+            }
+            Log.Info($"ServiceHost intialization completed. Found {factories.Length} plugins");
+        }
+
+        private static IServiceStrategyFactory[] GetStrategyFactories()
+        {
+            var pluginsFolder = GetPluginsFolder();
+            PluginLoader.LoadPlugins(pluginsFolder, Log);
+            return (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                    from assemblyType in domainAssembly.GetTypes()
+                    where typeof(IServiceStrategyFactory).IsAssignableFrom(assemblyType) && assemblyType.IsClass
+                    select assemblyType).ToArray().Select(Activator.CreateInstance)
+                .Select(instance => instance)
+                .Cast<IServiceStrategyFactory>()
+                .ToArray();
+        }
+
+        private static string GetPluginsFolder()
+        {
+            var setting = ConfigurationManager.AppSettings["PluginsFolder"] ?? "plugins";
+            var uri = new Uri(setting, UriKind.RelativeOrAbsolute);
+            return uri.IsAbsoluteUri ? uri.AbsolutePath : setting;
+        }
+    }
+}
